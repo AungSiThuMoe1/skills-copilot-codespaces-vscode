29c91bf412fda6bbcc4d2373a2bd31b63f0e7c12
Aung Si Thu Moe <aungsithumoe23@gmail.com>
2024-05-01 09:41:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        showSkills: function() {
+            this.skills.forEach(skill => {
+                console.log(`${this.name} knows ${skill}`);
+            });
+        }
+    };
+
+    member.showSkills();
+}
